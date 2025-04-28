4dff708e321059e11c0629a2044e0631d0e39663
HarrisonBounds <harrison.bounds777@gmail.com>
2025-04-28 09:14:12 -0500

    plotting done for original model

--- saved/model/starter.py
+++ saved/model/starter.py
@@ -429,7 +429,7 @@ def train_model(model, opt, tokenizer):
 
         # Save model
         if opt.savename:
-            torch.save(model.state_dict(), f"{opt.savename}/model_epoch_{epoch}.pth")
+            torch.save(model.state_dict(), f"{opt.savename}/model_epoch_{epoch+1}.pth")
 
         validate_model(model, opt, epoch, tokenizer)
         test_model(model, opt, epoch, tokenizer)
@@ -562,7 +562,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-no_cuda', action='store_true')
     parser.add_argument('-SGDR', action='store_true')
-    parser.add_argument('-epochs', type=int, default=5)
+    parser.add_argument('-epochs', type=int, default=20)
     parser.add_argument('-d_model', type=int, default=512)
     parser.add_argument('-n_layers', type=int, default=6)
     parser.add_argument('-heads', type=int, default=8)
@@ -572,7 +572,7 @@ def main():
     parser.add_argument('-lr', type=int, default=0.00001)
     parser.add_argument('-seqlen', type=int, default=512)
     parser.add_argument('-threshold', type=int, default=3)
-    parser.add_argument('-savename', type=str)    
+    parser.add_argument('-savename', type=str, default="model_weights")    
     parser.add_argument('-loadname', type=str)    
     parser.add_argument('-tied', type=int, default=1)
     parser.add_argument('-dir_name', type=str,default='model')
